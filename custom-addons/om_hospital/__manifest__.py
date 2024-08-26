0e83a9b24c03715c25490ec2e0a8f4fc4b2e9221
@Ramuo <barry.alpha88@outlook.fr>
2024-08-26 11:37:28 +0200

    statusbar

---
+++ custom-addons/om_hospital/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Hospital Management System",
+    "author": "Alpha BARRY",
+    "licence": "LGPL-3",
+    "version": "17.0.1.1",
+    "depends": [
+        'mail',
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/sequence.xml",
+        "views/patient_views.xml",
+        "views/patient_readonly_views.xml",
+        "views/appointment_views.xml",
+        "views/menu.xml",
+    ],
+}
